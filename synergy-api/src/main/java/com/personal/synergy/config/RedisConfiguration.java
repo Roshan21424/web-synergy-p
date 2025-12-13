6d4a3686877bf03cf492dd2c2819968a0ce9def3
Roshan21424 <roshanraj21042004@gmail.com>
2025-12-13 15:38:36 +0530

    feat(backend,redis): configure redis with template

---
+++ synergy-api/src/main/java/com/personal/synergy/config/RedisConfiguration.java
@@ -0,0 +1,32 @@
+package com.personal.synergy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Keys
+        template.setKeySerializer(StringRedisSerializer.UTF_8);
+        template.setHashKeySerializer(StringRedisSerializer.UTF_8);
+
+        RedisSerializer<Object> jsonSerializer = RedisSerializer.json();
+
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
